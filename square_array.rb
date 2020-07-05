529e5766cb8bfb9c0427b649e00be124fb56667a
Ynda Jas <yndajas@gmail.com>
2020-07-05 03:12:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,12 @@
 def square_array(array)
   squared_array = []
   array.each do |number|
-    squared_array << number*number
+    squared_array << number**2
   end
   squared_array
 end
 
 # refactored using collect
 # def square_array(array)
-#   array.collect {|number| number*number}
+#   array.collect {|number| number**2}
 # end
